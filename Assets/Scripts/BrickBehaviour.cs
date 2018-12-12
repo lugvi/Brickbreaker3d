b48d6c1a7fe8efb03c0e744eceac3a3ac15bb055
TedX <tedomakharadze95@gmail.com>
2018-12-12 18:32:04 +0400

    added

--- Assets/Scripts/BrickBehaviour.cs
+++ Assets/Scripts/BrickBehaviour.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 
 public class BrickBehaviour : MonoBehaviour
 {
@@ -9,11 +10,26 @@ public class BrickBehaviour : MonoBehaviour
     float hpoffset;
     public TMPro.TextMeshPro hpDisplay;
 
+    public Stack<GameObject> tower;
+
+    public GameObject topBrick;
+
+    public Material tempmat;
+
     public void Start()
     {
         MinHP = GameLogic.instance.MinHP;
         MaxHP = GameLogic.instance.MaxHP;
         HitPoints = Random.Range((int)MinHP, (int)MaxHP);
+        tower = new Stack<GameObject>();
+        for (int i = 0; i < HitPoints; i++)
+        {
+
+            GameObject cube = Instantiate(topBrick);
+            cube.transform.parent = this.transform.parent;
+            cube.transform.position = this.transform.position + Vector3.up * i * 2;//+Vector3.forward*40;
+            tower.Push(cube);
+        }
         hpoffset = (MaxHP - MinHP) / 4;
         ColorChange();
 
@@ -26,25 +42,37 @@ public class BrickBehaviour : MonoBehaviour
         if (HitPoints < MinHP + hpoffset)
         {
             GetComponent<MeshRenderer>().material = GameLogic.instance.colorDifficulty[0];
+            tempmat.color = GameLogic.instance.colorDifficulty[0].color;
         }
         else if (HitPoints < MinHP + hpoffset * 2)
         {
             GetComponent<MeshRenderer>().material = GameLogic.instance.colorDifficulty[1];
+            tempmat.color = GameLogic.instance.colorDifficulty[1].color;
         }
         else if (HitPoints <= MinHP * hpoffset * 3)
         {
             GetComponent<MeshRenderer>().material = GameLogic.instance.colorDifficulty[2];
+            tempmat.color = GameLogic.instance.colorDifficulty[2].color;
         }
         else
         {
             GetComponent<MeshRenderer>().material = GameLogic.instance.colorDifficulty[3];
+            tempmat.color = GameLogic.instance.colorDifficulty[3].color;
         }
+
     }
 
 
     public void OnHit()
     {
         int damage = GameLogic.instance.buffedDamage;
+
+        for (int i = 0; i < damage; i++)
+        {
+
+            Destroy(tower.Pop());
+
+        }
 
         HitPoints -= damage;
         if (HitPoints <= 0)
